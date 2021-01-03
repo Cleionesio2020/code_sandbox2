3f879d818593610c057b2fce950d03591e7e83f3
Cleionesio_Ribeiro <58114878+Cleionesio2020@users.noreply.github.com>
2021-01-03 16:29:54 -0300

    commit4 css_login adicionado

--- src/Login.js
+++ src/Login.js
@@ -1,43 +1,43 @@
 import React from "react";
+import "./login.css";
 
 export default function () {
   return (
-    <div>
-      <form
-        style={{ width: 400, margin: "auto", marginTop: 100, padding: 30 }}
-        className="card"
-      >
-        <div className="form-group">
-          <h3 style={{ textAlign: "center" }}>Login</h3>
-          <input
-            type="email"
-            className="form-control"
-            id="exampleDropdownFormEmail2"
-            placeholder="email@example.com"
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="password"
-            className="form-control"
-            id="exampleDropdownFormPassword2"
-            placeholder="Password"
-          />
-        </div>
-        <div className="form-group">
-          <div className="form-check">
+    <div className="card-login">
+      <div className="card">
+        <form form>
+          <div className="form-group">
+            <h3 style={{ textAlign: "center" }}>Login</h3>
             <input
-              type="checkbox"
-              className="form-check-input"
-              id="dropdownCheck2"
+              type="email"
+              className="form-control"
+              id="exampleDropdownFormEmail2"
+              placeholder="email@example.com"
             />
-            <label className="form-check-label">Remember me</label>
           </div>
-        </div>
-        <button type="submit" className="btn btn-primary btn-block">
-          Logar no sistema
-        </button>
-      </form>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="exampleDropdownFormPassword2"
+              placeholder="Password"
+            />
+          </div>
+          <div className="form-group">
+            <div className="form-check">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="dropdownCheck2"
+              />
+              <label className="form-check-label">Remember me</label>
+            </div>
+          </div>
+          <button type="submit" className="btn btn-primary btn-block">
+            Logar no sistema
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
